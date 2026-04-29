b9112d07b0b95a80419e1b0da249f907259ad6a3
rodinez <mngtandrade@gmail.com>
2026-04-28 21:10:46 -0300

    UPDATE: creation of the .objects directory

--- main.c
+++ main.c
@@ -9,6 +9,20 @@
 #include "lib/conf.h"
 
 int main(){
+    struct stat stats;
+    char *objects_dir = ".objects";
+
+   if (stat(objects_dir, &stats) != 0) {
+        if (mkdir(objects_dir, 0755) != 0){
+            fprintf(stderr, "Failed to create .objects\n");
+            perror("mkdir");
+            exit(EXIT_FAILURE);
+        }
+    } else if (!S_ISDIR(stats.st_mode)) {
+        fprintf(stderr, "Path to .objects already taken\n");
+        exit(EXIT_FAILURE);
+    } 
+
     node *root = create_tree("repo");
     char path[PATH_LEN] = "repo";
     DIR *dir = opendir(path);
@@ -17,7 +31,7 @@ int main(){
     root = read_dir(dir, repo, root, path);
     calculate_tree_hash(root);
     
-    commit *first_commit = create_commit(root->hash, author, message);
+    commit *first_commit = create_commit(NULL, root->hash, author, message);
 
     print_files(root);
     destroy_nodes(root);
